802e204b2c806bc272088a59526eb5e703953dd9
komluk <lukasz.komosa@komluk.com>
2019-10-26 18:22:43 +0200

    Empty solution

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KomLuk.Hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
