a5be35c2de2c09d2d627c2cef7697563e7bdc8c3
komluk <lukasz.komosa@komluk.com>
2019-10-27 20:17:58 +0100

    sorting

---
+++ src/DataStructures/UnitTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DataStructures
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void MatchingStrings()
+        {
+            string[] strings = new string[] { "aba", "baba", "aba", "xzxb" };
+
+            string[] queries = new string[] { "aba", "xzxb", "ab" };
+
+
+            var sList = strings.ToList();
+            int[] ar = new int[queries.Length];
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int count = strings.Count(x => x == queries[i]);
+                ar[i] = count;
+            }
+
+            // Assert.Equals(2, ar[0]);
+            // Assert.Equals(1, ar[1]);
+            // Assert.Equals(0, ar[2]);
+        }
+
+        [Fact]
+        public void CountSwaps()
+        {
+            int[] a = new int[] { 3, 2, 1 };
+            int n = a.Length;
+            int swaps = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int left = a[j];
+                        int right = a[j + 1];
+                        a[j] = right;
+                        a[j + 1] = left;
+                        swaps++;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -6,20 +6,40 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 4, 3, 1, 2 };
-            int swap = 0;
-            for (int i = 0; i < arr.Length; i++)
+            // int[] arr = new int[] { 4, 3, 1, 2 };
+            // int swap = 0;
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            //     while (arr[i] != i + 1)
+            //     {
+            //         var temp = arr[arr[i] - 1];
+            //         arr[arr[i] - 1] = arr[i];
+            //         arr[i] = temp;
+            //         swap += 1;
+            //     }
+            // }
+
+            // Console.WriteLine(swap);
+
+            int[] a = new int[] { 3, 2, 1 };
+            int n = a.Length;
+            int swaps = 0;
+            for (int i = 0; i < n; i++)
             {
-                while (arr[i] != i + 1)
+                for (int j = 0; j < n - 1; j++)
                 {
-                    var temp = arr[arr[i] - 1];
-                    arr[arr[i] - 1] = arr[i];
-                    arr[i] = temp;
-                    swap += 1;
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+                        int min = a[j];
+                        int max = a[j + 1];
+                        a[j] = max;
+                        a[j + 1] = min;
+                        swaps++;
+                    }
                 }
             }
 
-            Console.WriteLine(swap);
         }
     }
 }
